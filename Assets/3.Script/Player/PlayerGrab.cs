d7a027cde4909c61790ac5e9c7931ef660b91eaa
M1nah <p9me0615@gmail.com>
2023-06-22 18:02:33 +0900

    [Fix] 01 Grab 스크립트 수정

--- Assets/3.Script/Player/PlayerGrab.cs
+++ Assets/3.Script/Player/PlayerGrab.cs
@@ -7,6 +7,7 @@ public class PlayerGrab : MonoBehaviour
     PlayerInput playerInput;
     PlayerController playerController;
 
+    bool isUp=false;
 
     private void Awake()
     {
@@ -14,5 +15,29 @@ public class PlayerGrab : MonoBehaviour
         playerController = GetComponent<PlayerController>();
     }
 
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.CompareTag("GrabPlatform"))
+        {
+            isUp = true;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D col)
+    {
+        if (col.CompareTag("GrabPlatform"))
+        {
+            isUp = false;
+        }
+    }
+
+    private void GrabHill()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            //사다리 응용
+            //transform.position += new Vector2(transform.position.y, 0f);
+        }
+
+    }
 
 }
--- Assets/3.Script/Player/PlayerInput.cs
+++ Assets/3.Script/Player/PlayerInput.cs
@@ -4,22 +4,15 @@ using UnityEngine;
 
 public class PlayerInput : MonoBehaviour
 {
-    [SerializeField] private string MoveAxisName = "Vertical";
-    [SerializeField] private string RotateAxisName = "Horizontal";
-    public float Move_Value { get; private set; }
-    public float Rotate_Value { get; private set; }
     public bool isInteraction { get; private set; }
     public bool isJump { get; private set; }
 
     // Update is called once per frame
     void Update()
     {
-        Move_Value = Input.GetAxis(MoveAxisName);
-        Rotate_Value = Input.GetAxis(RotateAxisName);
 
         isInteraction = Input.GetKeyDown(KeyCode.E);
         isJump = Input.GetKeyDown(KeyCode.Space);
-
 
     }
 }
