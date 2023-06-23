ba77a37f66e03cda0c8b0c6f9b2ad763a89b60b6
M1nah <p9me0615@gmail.com>
2023-06-23 11:39:15 +0900

    [Fix] 00 Intro Panel 수정

--- Assets/3.Script/CursorPoint.cs
+++ Assets/3.Script/CursorPoint.cs
@@ -5,44 +5,11 @@ using UnityEngine;
 public class CursorPoint : MonoBehaviour
 {
     public Texture2D cursorArrow;
-    public Texture2D cursorPick;
+    [HideInInspector] public Texture2D cursorPick;
 
     // Start is called before the first frame update
     void Start()
     {
         Cursor.SetCursor(cursorArrow, Vector2.zero, CursorMode.ForceSoftware);
     }
-
-    private void Update()
-    {
-        PauseCursor();
-    }
-
-    private void PauseCursor()
-    {
-        if (Input.GetMouseButtonDown(0))
-        {
-            Cursor.SetCursor(cursorPick, Vector2.zero, CursorMode.ForceSoftware);
-        }
-
-        if (Input.GetMouseButtonDown(0))
-        {
-            Cursor.SetCursor(cursorArrow, Vector2.zero, CursorMode.ForceSoftware);
-        }
-    }
-
-    //private void OnMouseEnter(Collider2D col)
-    //{
-    //    if (col.CompareTag("Button"))
-    //    {
-    //        Cursor.SetCursor(cursorPick, Vector2.zero, CursorMode.ForceSoftware);
-    //    }
-    //}
-    //private void OnMouseExit(Collider2D col)
-    //{
-    //    if (col.CompareTag("Button"))
-    //    {
-    //        Cursor.SetCursor(cursorArrow, Vector2.zero, CursorMode.ForceSoftware);
-    //    }
-    //}
 }
