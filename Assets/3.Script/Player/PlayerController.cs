69cbe28c82a431f9e55ae2380e29dc2fdfa3a088
M1nah <p9me0615@gmail.com>
2023-06-29 20:47:25 +0900

    [Fix]01 playerController 스크립트 수정

--- Assets/3.Script/Player/PlayerController.cs
+++ Assets/3.Script/Player/PlayerController.cs
@@ -10,7 +10,7 @@ public class PlayerController : MonoBehaviour
     SpriteRenderer spriteRenderer; //방향전환을 위한 변수
 
     //move
-    [SerializeField] float moveSpeed;
+    [SerializeField] float moveSpeed=3f;
     [SerializeField] float maxSpeed;
 
     //jump
@@ -72,29 +72,38 @@ public class PlayerController : MonoBehaviour
             armAni.SetBool("ArmIsJumping", false);
         }
 
-
     }
 
     private void FixedUpdate()
     {
+        //Climing();
 
-        if (playerInput.isMoveUp)
+
+        if (isCliming)
         {
-            Climing();
-
-            if (isCliming)
-            {
-
-                rigid.gravityScale = 0f; //벽에 닿았을 때 중력을 0으로
-                rigid.velocity = new Vector2(rigid.velocity.x, vertical * moveSpeed);
-            }
-            else if (!isCliming)
-            {
-                rigid.gravityScale = 3f; //아닐때 중력을 되돌리기 
-            }
-
+            vertical = Input.GetAxis("Vertical");
+            rigid.gravityScale = 0f;
+            rigid.velocity = new Vector2(rigid.velocity.x, vertical * 3); //3 = moveSpeed였음 
+            //아래 stopSpeed 부분에서 멈추느라 속도를 0으로 만들어버려서 그뒤로 쭉 moveSpeed에 0이 적용된다...
+        }
+        else
+        {
+            rigid.gravityScale = 3f;
         }
 
+        //if (isCliming)
+        //{
+        //    rigid.gravityScale = 0f; //벽에 닿았을 때 중력을 0으로
+        //    rigid.velocity = new Vector2(rigid.velocity.x, vertical * moveSpeed);
+        //    Debug.Log("벽 오르기");
+        //}
+        //
+        //else if(!(isCliming))
+        //{
+        //    isCliming = false;
+        //    rigid.gravityScale = 3f; //아닐때 중력을 되돌리기 
+        //    Debug.Log("벽에서 떨어짐"); //걍 isMoveUp 키만 눌러도 여기가 무한적으로 올라가는구만..
+        //}
     }
 
 
@@ -129,7 +138,7 @@ public class PlayerController : MonoBehaviour
         //stop Speed
         if (Input.GetButtonUp("Horizontal"))
         {
-            moveSpeed = 0;
+            moveSpeed = 0f; //여기 스피드가 0이 되느라 climb.y 값을 구할떄 0이 곱해진다... 그렇다면 어케해야하느냐... 
         }
 
     }
@@ -149,7 +158,7 @@ public class PlayerController : MonoBehaviour
         //    jumpCount = 0;
         //    Debug.Log("jump stop"); //isGround= false 면 jumpCount= 0으로 바꿈... 그런데 여기가 안들어가짐..
         //                            //그렇지만 작동하는걸... 된거 아닐까? 여기 필요 없는거 아냐?
-        //                            //주석 처리해도 작동하잖아! 필요 없었잔아! 
+        //                            //주석 처리해도 작동하잖아! 필요 없었잔아! @@
         //}
     }
 
@@ -165,7 +174,7 @@ public class PlayerController : MonoBehaviour
                                      //isGround bool값을 만들어주고 체크하니까 이제 리셋 됨
                                      //그리고 공중에서 점프 안하게 됨 해결 !  -> 근대 오ㅔ 또 애니메이션 고장난거야
                                      // -> jumpCount++ 되는 곳에 isGround 체크해줬더니 애니메이션 또 됨 완벽하게 해결! 
-                                     //Running 도중 jumping ani로 바뀌는 건 그냥 트렌지션에 Running 값도 true로 바꿔줬더니 잘만 나오더라 하...
+                                     //Running 도중 jumping ani로 바뀌는 건 그냥 트렌지션에 Running 값도 true로 바꿔줬더니 잘만 나오더라 하... @@
         }
     }
     private void OnCollisionExit2D(Collision2D collision)
@@ -183,6 +192,8 @@ public class PlayerController : MonoBehaviour
         {
             isCliming = true;
         }
+
+       
     }
 
 
