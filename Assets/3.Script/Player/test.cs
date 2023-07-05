9eae7675c85d3ef93ba471a29e2d71b7d04a4a8f
M1nah <p9me0615@gmail.com>
2023-07-05 11:27:40 +0900

    [Create] Hook 스크립트 추가, [Fix] playerGrab ->playerHookShot 이름 수정, 스크립트 수정

--- Assets/3.Script/Player/test.cs
+++ Assets/3.Script/Player/test.cs
@@ -2,37 +2,90 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class test : MonoBehaviour //Hookshot 
+public class test : MonoBehaviour //Hookshot(player)
 {
-    public LineRenderer line;
+    [SerializeField] GameObject GrabHook;
     public Transform hook;
+    public LineRenderer line;
 
-    Vector2 mouseDirection;
+    Vector2 mouseDirection; //마우스 커서 위치
+
+    public bool isHookActive; //마우스 버튼이 눌렸을 때 참으로 바꿔주는 변수
+    public bool isLineMax;
+    public bool isAttach; // 이 변수가 참일 때 platform에 붙는다 linemax가 발동 안함 
 
     private void Start()
     {
         line.positionCount = 2;
-        line.endWidth = line.startWidth = 0.05f;
-        line.SetPosition(0,transform.position);
+        line.endWidth = line.startWidth = 0.02f;
+        line.SetPosition(0, transform.position);
         line.SetPosition(1, hook.position);
-        line.useWorldSpace = true;//라인렌더러가 추가되어있는 오브젝트의 위치와 상관없이 월드 좌표를 기준으로 화면에 라인이 그려지게 됨
+
+        line.useWorldSpace = true;
+        //↑라인렌더러가 추가되어있는 오브젝트의 위치와 상관없이
+        //월드 좌표를 기준으로 화면에 라인이 그려지게 됨
+
+        isAttach = false;
     }
 
     private void Update()
     {
+        //hook와 player의 위치가 바뀌면 포지션이 변경되게
         line.SetPosition(0, transform.position);
         line.SetPosition(1, hook.position);
 
-        if (Input.GetMouseButtonDown(0))
-        {
-            hook.position = transform.position;
-            mouseDirection = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
-        
-        }
-        else if (Input.GetMouseButtonUp(0))
+        if (Input.GetMouseButtonDown(0) && !isHookActive)
         {
 
+            //hoook는 player의 위치에서 날아가야하니까
+            //마우스 버튼을 눌렀을 때 hook의 위치를 player의 위치로 초기화 시켜준다
+            hook.position = transform.position;
+
+            //마우스 포지션은 스크린 기준으로 값을 반환하니까 월드 좌표로 바꿔준 뒤
+            //player의 위치값을 빼주면 고리가 날아가는 방향의 백터값을 알수 있다
+            mouseDirection = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+            //=> 그렇다면 main 기준이 아니라 cinemachineCamera 기준으로 바꿔보면 어떨까? 되나? 
+
+            isHookActive = true;
+            isLineMax = false;
+            GrabHook.SetActive(true);
+            Debug.Log("Hook 발사");
         }
+
+        if (isHookActive && !isLineMax && !isAttach) //isHookActive가 참이고 lineMax가 거짓일 때만 후크가 날아가게끔 하기
+        {
+            hook.Translate(mouseDirection.normalized * Time.deltaTime * 8);
+
+            if (Vector2.Distance(transform.position, hook.position) > 2) //Distance(a,b)=> a에서 b까지의 거리구하기함수
+            {
+                //hook와 player의 거리가 5보다 클 때 lineMax는 참이다
+                isLineMax = true;
+            }
+        }
+        else if (isHookActive && isLineMax && !isAttach)
+        { //마우스 버튼을 떼고 isHookActive가 참이고 line이 최고지점에 도달했을 때(isLineMax가 참일때) 후크가 돌아옴
+            hook.position = Vector2.MoveTowards(hook.position, transform.position, Time.deltaTime * 8);
+
+            if (Vector2.Distance(transform.position, hook.position) < 0.1f)
+            { //hook와 player의 간격이 0.1보다 작아지면 isHookActive와 isLineMax를 비활성화 => 고리가 움직이지 않게 한다 
+                isHookActive = false;
+                isLineMax = false;
+                GrabHook.SetActive(false);
+                Debug.Log("hook 회수");
+            }
+        }
+        else if (isAttach) //platform에서 후크가 떨어질 때 모든 bool 변수가 비활성화
+        {
+            if (Input.GetMouseButtonUp(0))
+            {
+                isAttach = false;
+                isHookActive = false;
+                isLineMax = false;
+                GrabHook.GetComponent<test2>().joint2D.enabled = false;
+                GrabHook.SetActive(false);
+            }
+        }
+
     }
 
 }
--- Assets/3.Script/Player/test2.cs
+++ Assets/3.Script/Player/test2.cs
@@ -2,221 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class test2 : MonoBehaviour //playerController에 test 스크립트를 합쳐보자
+public class test2 : MonoBehaviour //Hookshot(Hook)
 {
-    public PlayerInput playerInput;
-    [SerializeField] Rigidbody2D rigid; //물리 이동을 위한 변수 선언
-    SpriteRenderer spriteRenderer; //방향전환을 위한 변수
+    [SerializeField] test grapping;
+    [SerializeField] public DistanceJoint2D joint2D;
 
-    //move
-    [Header("Move")]
-    [SerializeField] float moveSpeed = 3f;
-    [SerializeField] float maxSpeed;
-
-    //jump
-    [Header("Jump")]
-    [SerializeField] public float jump;
-    int jumpCount = 0;
-
-    bool isJump = false;
-    bool isGround = false; //점프하고 바닥에 닿았는지 체크 ->이거 없애도 되겠다.. 이유: 이미 Tag로 체크하고있음
-
-    //Climing && Slide
-    [Header("Climing && Slide")]
-    [SerializeField] Transform wallCheck;
-    [SerializeField] float slidingSpeed;
-    [SerializeField] float wallChkDistance;
-    float isRight = 1f; //바라보는 방향 1= right , -1 = Left
-    
-    [SerializeField] LayerMask wallLayer; //레이캐스트 겁출 레이어
-
-    bool isWall;
-    bool isWallStay = false;
-
-    //Animation player + Arm
-    [Header("Animation player && Arm")]
-    public GameObject Arm;
-    Animator playerAni;
-    Animator armAni;
-
-
-    private void Awake()
+    private void Start()
     {
-        playerInput = GetComponent<PlayerInput>();
-        rigid = GetComponent<Rigidbody2D>();
-        spriteRenderer = GetComponent<SpriteRenderer>();
-
-        playerAni = GetComponent<Animator>();
-        armAni = Arm.GetComponent<Animator>();
+        //grapping은 "Player" 태그가 붙은 GameObject를 찾아서 거기에 붙은 test 컴포넌트를 가져오다는 뜻
+        //직접 변수를 할당해줘도 괜춘함
+        //grapping = GameObject.Find("Player").GetComponent<test>();
+        //joint2D = GetComponent<DistanceJoint2D>();
     }
 
-    private void Update()
-    {
-
-        //move
-        if (playerInput.isMoveLeft || playerInput.isMoveRight)
-        {
-            Climb_Ray();
-            Move();
-            playerAni.SetBool("isRunning", true);
-            armAni.SetBool("ArmIsRunning", true);
-
-        }
-        else
-        {
-            playerAni.SetBool("isRunning", false);
-            armAni.SetBool("ArmIsRunning", false);
-        }
-
-        //jump
-        if (playerInput.isJump)
-        {
-            Jump();
-            playerAni.SetBool("isJumping", true);
-            armAni.SetBool("ArmIsJumping", true);
-        }
-        else if (!playerInput.isJump && !isJump)
-        {
-            playerAni.SetBool("isJumping", false);
-            armAni.SetBool("ArmIsJumping", false);
-        }
-
-    }
-
-    private void FixedUpdate()
-    {
-        if (isWall && isWallStay) //Wall에 Raycast를 쐈고 Wall collider에 닿았을 때
-        {
-
-            if (playerInput.isMoveUp) // input값을 받아서
-            {
-                Debug.Log("climb 시작..혹시 여기니?"); //여기 자체를 못들어가고있는데 머임
-                float ver = Input.GetAxis("Vertical");
-                rigid.velocity = new Vector2(rigid.velocity.x, ver * slidingSpeed); //리지드바디가 위로 움직임
-                //근데 지금 Down키가 안먹힘
-                playerAni.SetBool("isWallCilmbUp", true);
-                armAni.SetBool("ArmIsWallClimbUp", true);
-                Debug.Log("climb 시작");
-
-            }
-            ////이건 천천히 미끄러지는거 지울거임
-            //wallRgd.velocity = new Vector2(wallRgd.velocity.x, wallRgd.velocity.y * slidingSpeed);
-            //Debug.Log("벽에 닿았으며 미끄러져내려감"); //작동은 하는데 천천히 미끄러지지가 않음 
-            //왼쪽(-)은 작동하는데 오른쪽(+)은 작동안함
-            //collider가 너무 얇아서 검출 안되던 거였다...box로 바꾸니 잘됨 이런젠장
-            // 그렇다면 왼쪽 collider는 두꺼웠던것인가
-        }
-    }
-
-
-    private void Move()
-    {
-        //player Movement
-        float h = Input.GetAxisRaw("Horizontal");
-        rigid.AddForce(Vector2.right * h, ForceMode2D.Impulse);
-
-        #region maxSpeed 관리
-        if (rigid.velocity.x > maxSpeed) //maxSpeed를 넘으면
-        {
-            //해당 오브젝트의 속력은 maxSpeed
-            rigid.velocity = new Vector2(maxSpeed, rigid.velocity.y);
-        }
-        else if (rigid.velocity.x < maxSpeed * (-1))
-        {
-            rigid.velocity = new Vector2(maxSpeed * -1, rigid.velocity.y);
-        }
-        #endregion
-
-        //Direction Sprite Flip(with Child Component)
-        if (playerInput.isMoveLeft)
-        {
-            spriteRenderer.transform.localScale = new Vector2(-1, 1);
-        }
-        else if (playerInput.isMoveRight)
-        {
-            spriteRenderer.transform.localScale = new Vector2(1, 1);
-        }
-
-        //stop Speed
-        if (playerInput.isMoveLeft == true || playerInput.isMoveRight == true)
-        {
-            moveSpeed = 3f;
-        }
-        else
-        {
-            moveSpeed = 0f;
-        }
-
-    }
-    private void Jump()
-    {
-        if (jumpCount < 1)
-        {
-            isGround = true;
-            jumpCount++;
-            rigid.AddForce(Vector2.up * jump, ForceMode2D.Impulse);
-            isJump = true;
-        }
-    }
-
-
-    private void Climb_Ray()
-    {
-        if (playerInput.isMoveRight)
-        {
-            isWall = Physics2D.Raycast(wallCheck.position, Vector2.right * isRight, wallChkDistance, wallLayer);
-            //2D에서는 기본적으로 오른쪽을 바라보고 있기 때문에 Vetor2D의 값을 right로 지정한다
-            Debug.DrawRay(wallCheck.position, Vector2.right * isRight, Color.red);
-        }
-        if (playerInput.isMoveLeft)
-        {
-            isWall = Physics2D.Raycast(wallCheck.position, Vector2.left, wallChkDistance, wallLayer);
-            Debug.DrawRay(wallCheck.position, Vector2.left, Color.blue);
-        }
-    }
-
-    //jumpCount Reset 
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject.CompareTag("Platform"))
-        {
-            isGround = true;
-            isJump = false;
-            jumpCount = 0;
-            Debug.Log("jump reset"); //점프 리셋이 전혀 안되는디
-                                     //isGround bool값을 만들어주고 체크하니까 이제 리셋 됨
-                                     //그리고 공중에서 점프 안하게 됨 해결 !  -> 근대 오ㅔ 또 애니메이션 고장난거야
-                                     // -> jumpCount++ 되는 곳에 isGround 체크해줬더니 애니메이션 또 됨 완벽하게 해결! 
-                                     //Running 도중 jumping ani로 바뀌는 건 그냥 트렌지션에 Running 값도 true로 바꿔줬더니 잘만 나오더라 하... @@
-        }
-
-    }
-
-    //Climb Check
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("GrabPlatform") && isWall)
+        if (collision.CompareTag("GrabPlatform"))
         {
-            isWallStay = true;
-        }
-    }
-    private void OnTriggerStay2D(Collider2D collision)
-    {
-        if (collision.CompareTag("GrabPlatform") && isWall)
-        {
-            isWallStay = true;
-        }
-
-        if (collision.CompareTag("GrabPlatform") && playerInput.isJump)
-        {
-            isWallStay = false;
-        }
-    }
-    private void OnTriggerExit2D(Collider2D collision)
-    {
-        if (collision.CompareTag("GrabPlatform") && playerInput.isJump)
-        {
-            isWallStay = false;
+            joint2D.enabled = true;
+            grapping.isAttach = true;
         }
     }
 
